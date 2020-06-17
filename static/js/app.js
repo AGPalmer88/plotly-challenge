ce502180224a537c724bfb440827f7384fedbfbb
AGPalmer88 <61656034+AGPalmer88@users.noreply.github.com>
2020-06-16 21:28:05 -0400

    commit

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,28 @@
-// Step1: Plotly
+// Step1: Plotly 
+/// BAR CHART 
   // Use the D3 library to load in samples.json.
-  d3.json("samples.json").then((incomingData) => {
-    consoled.log(data);
-});
+  d3.json("samples.json").then(function(data) {
+
+    var sample_values = data.sample_values;
+    console.log(sample_values);
+    var otu_labels = data.otu_labels;
+    console.log(otu_labels);
+    var otu_ids = data.otu_ids;
+    console.log(otu_ids);
+
+    var data = [{
+      values: sample_values,
+      labels: otu_ids,
+      hovertext: otu_labels,
+      type: 'bar'
+    }];
+
+    var layout = {
+      height: 400,
+      width: 500
+    };
+
+    Plotly.newPlot('bar-plot', data, layout);
+
+    });
+
